d8524ccd569f368ed2db564fb584268cc5248180
rodrigocflorindo <rodrigocflorindo@gmail.com>
2024-03-12 09:24:58 -0300

    newpage

--- user-data.sh
+++ user-data.sh
@@ -5,16 +5,5 @@ yum update -y
 yum install -y httpd
 systemctl start httpd
 systemctl enable httpd
-#echo "<h1>Aplicação Multicamadas na AWS para Demonstração Servidor: $(hostname -f)</h1>" > /var/www/html/index.html
-
-echo "<!DOCTYPE html>
-<html>
-<head>
-  <title>HMulticamadas na AWS</title>
-</head>
-<body>
-  <h1>Aplicação Multicamadas na AWS para Demonstração > Servidor: $(hostname -f)</h1>
-  <p>Arquitetura Simples</p>
-  <img src=\"https://rcfimages.s3.amazonaws.com/Diagrama-de-Arquitetura-AWS.png" alt=\"Example Image\">
-</body>
-</html>" | sudo tee /var/www/html/index.html
+echo "<h1>Aplicação Multicamadas na AWS para Demonstração Servidor: $(hostname -f)</h1>" > /var/www/html/index.html
+echo "<img src=\"https://rcfimages.s3.amazonaws.com/Diagrama-de-Arquitetura-AWS.png">" >> /var/www/html/index.html
