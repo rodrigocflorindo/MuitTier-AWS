91410f534df34d5bbb3db8111cf3ce93cc2fd01c
rodrigocflorindo <rodrigocflorindo@gmail.com>
2024-03-12 10:35:49 -0300

    add variable provider e newpage

--- provider.tf
+++ provider.tf
@@ -1,6 +1,8 @@
 provider "aws" {
   region = var.region-name
-  access_key = "AKIA42B6SBETMHFK6J7G"
-  secret_key = "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"
+  #access_key = "AKIA42B6SBETMHFK6J7G"
+  #secret_key = "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"
+  shared_credentials_file = "/mnt/c/Users/rodri/.aws/credentials"
+  profile                 = "aws-terraform"
 }
 
--- user-data.sh
+++ user-data.sh
@@ -6,4 +6,4 @@ yum install -y httpd
 systemctl start httpd
 systemctl enable httpd
 echo "<h1>Aplicação Multicamadas na AWS para Demonstração Servidor: $(hostname -f)</h1>" > /var/www/html/index.html
-echo "<img src=\"https://rcfimages.s3.amazonaws.com/Diagrama-de-Arquitetura-AWS.png">" >> /var/www/html/index.html
+echo "<img src=\"https://rcfimages.s3.amazonaws.com/Diagrama-de-Arquitetura-AWS.png\">" >> /var/www/html/index.html
