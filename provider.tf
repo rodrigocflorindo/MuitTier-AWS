ba3820cf97bc8359fbbb73c4b1b1ed6ff88be8f9
rodrigocflorindo <rodrigocflorindo@gmail.com>
2024-03-11 21:50:27 -0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region = var.region-name
-  access_key = "AKIA42B6SBETMHFK6J7G"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
 
